5b906baf25b851c3a15eca7919c6103a64b47fb7
CarbonEternity <pa03622789@gmail.com>
2020-05-09 21:55:09 +0300

    system tray icon

---
+++ Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace SystemTray
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenu1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.mnuShow = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuHide = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenu1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenu1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Область уведомлений";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
+            // 
+            // contextMenu1
+            // 
+            this.contextMenu1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuShow,
+            this.mnuHide});
+            this.contextMenu1.Name = "contextMenu1";
+            this.contextMenu1.Size = new System.Drawing.Size(125, 48);
+            // 
+            // mnuShow
+            // 
+            this.mnuShow.Name = "mnuShow";
+            this.mnuShow.Size = new System.Drawing.Size(124, 22);
+            this.mnuShow.Text = "Показать";
+            this.mnuShow.Click += new System.EventHandler(this.mnuShow_Click);
+            // 
+            // mnuHide
+            // 
+            this.mnuHide.Name = "mnuHide";
+            this.mnuHide.Size = new System.Drawing.Size(124, 22);
+            this.mnuHide.Text = "Скрыть";
+            this.mnuHide.Click += new System.EventHandler(this.mnuHide_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(227, 126);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.contextMenu1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenu1;
+        private System.Windows.Forms.ToolStripMenuItem mnuShow;
+        private System.Windows.Forms.ToolStripMenuItem mnuHide;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace SystemTray
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.ShowInTaskbar = false;
+        }
+
+        private void mnuShow_Click(object sender, EventArgs e)
+        {
+            //Включаем отображения приложения на панели задач при запуске
+            this.ShowInTaskbar = true;
+            //Показываем форму
+            this.Show();
+            //Отключаем доступность пункта меню mnuShow
+            mnuShow.Enabled = false;
+            //Включаем доступность пункта меню mnuHide
+            mnuHide.Enabled = true;
+            //Переменной Hidden устанавливаем значение false
+        }
+
+        private void mnuHide_Click(object sender, EventArgs e)
+        {
+            this.ShowInTaskbar = false;
+            this.Hide();
+            mnuShow.Enabled = true;
+            mnuHide.Enabled = false;
+        }
+
+        private void notifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+            mnuShow_Click(this, new EventArgs());
+        }
+    }
+}
